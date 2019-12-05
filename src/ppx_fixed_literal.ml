1554c745c4ba6c84448b546e240797eeb5c19388
Xavier Clerc <xclerc@janestreet.com>
2019-12-05 09:33:14 +0000

    v0.14-preview.121.15+112

--- src/ppx_fixed_literal.ml
+++ src/ppx_fixed_literal.ml
@@ -21,13 +21,21 @@ let string_to_v loc s =
       ~loc
       "Unsupported literal for Fixed_literal %S@,Only decimal literals are supported"
       s;
-  let decimals, no_decimal_string =
+  let decimal_part, no_decimal_string =
     match String.lsplit2 ~on:'.' s with
-    | None -> 0, s
-    | Some (integer_part, decimal_part) ->
-      String.length decimal_part, integer_part ^ decimal_part
+    | None -> "", s
+    | Some (integer_part, decimal_part) -> decimal_part, integer_part ^ decimal_part
   in
+  let decimals = String.length decimal_part in
   let gloc = { loc with loc_ghost = true } in
+  let validate_digit_for_precision body =
+    let loc = gloc in
+    let precision = String.rstrip decimal_part ~drop:(Char.equal '0') |> String.length in
+    let name = Printf.sprintf "With_%i_digits_precision" precision in
+    [%expr
+      let (_ : Fixed_literal.precision) = [%e pexp_variant ~loc name None] in
+      [%e body]]
+  in
   (* Only the application node gets a real location, so when we ask merlin for
      the type of the literal in the source, we get [Fixed.t], not [int]. *)
   let function_expr =
@@ -41,6 +49,7 @@ let string_to_v loc s =
     ~loc
     function_expr
     [ Nolabel, pexp_constant ~loc:gloc (Pconst_integer (no_decimal_string, None)) ]
+  |> validate_digit_for_precision
 ;;
 
 let () =
